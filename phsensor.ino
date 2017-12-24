5010ff3832ee6554c1e2cc1d7b71ebe53a1b3d84
Harin Sanghirun <harin.sanghirun@gmail.com>
2017-12-24 13:03:58 +0700

    add dht11 reading

--- phsensor.ino
+++ phsensor.ino
@@ -1,26 +1,25 @@
-#include <Wire.h> 
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 
 #define PH_401_VOLTAGE 3.00
 #define PH_700_VOLTAGE 2.20
 #define PH_SENSOR_PIN A0
+#define DHT_PIN 13
+#define DHT_TYPE DHT11
 
 double m = 2.99 / ( PH_700_VOLTAGE - PH_401_VOLTAGE);
 double b = 7 - ( m * PH_700_VOLTAGE );
 int buf[10],temp;
+DHT dht = DHT(DHT_PIN, DHT_TYPE);
 
 void setup() {
   Serial.begin(9600);
   pinMode(PH_SENSOR_PIN, INPUT);
-
-  Wire.begin();
+  dht.begin();
 }
 
-void loop() {
-
-//  double value = analogRead(PH_SENSOR_PIN);
-//  double voltage = value * 5.0/ 1024.0;
-
-  // read 10 values 
+void readPH() {
+    // read 10 values 
   for(int i = 0; i < 10; i++) {
     buf[i] = analogRead(PH_SENSOR_PIN);
     delay(10);
@@ -51,13 +50,23 @@ void loop() {
   Serial.print("voltage = ");
   Serial.print(voltage, 4);
   Serial.print("\t pH = ");
-  Serial.println(pH, 4);
-  
-  // Transmit data
-  Wire.beginTransmission(9);
-  String phStr = String(pH, 4);
-  Wire.write((int) 0);
-  Wire.endTransmission();
-  
+  Serial.print(pH, 4);
+}
+
+void readTemp() {
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  Serial.print("temp = ");
+  Serial.print(t);
+  Serial.print("\t humidity = ");
+  Serial.print(h);
+}
+
+void loop() {
+  readPH();
+  Serial.print("\t");
+  readTemp();
+  Serial.print("\n");
   delay(1000);
 }
